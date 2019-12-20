8277973788f2d37649395ec3a09db9bc0d42e897
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-12-20 11:25:45 -0300

    Password

---
+++ week1-022.Password/src/Password.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Password {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String password = "carrot"; // Use carrot as password when running tests.
+
+        // Write your code here
+        while (true){
+            System.out.println("Type the password: ");
+            password = reader.nextLine();
+            if(password.equals("carrot")){
+                System.out.println("Right!");
+                System.out.println(" ");
+                System.out.println("The secret is: jryy qbar!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+    }
+}
