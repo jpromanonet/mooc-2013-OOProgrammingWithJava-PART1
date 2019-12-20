d65dc5bbfc8136a037615874528aac548e1bbd1c
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-12-20 10:44:44 -0300

    Circle Circumference

---
+++ week1-010.CircleCircumference/src/Circumference.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class Circumference {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Program your solution here
+        System.out.println("Type the radius: ");
+        double radius = Integer.parseInt(reader.nextLine());
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Circumference of the circle: "+ circumference);
+    }
+}
